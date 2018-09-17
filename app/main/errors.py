585aa655d4a773096c3650d35685d6274f60e6be
kevinjared <kevinjaredkip@gmail.com>
2018-09-17 15:13:21 +0300

    errors file

---
+++ app/main/errors.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from . import main
+
+@main.app_errorhandler(404)
+def not_found(error):
+    '''
+    function to render the 404 error page
+    '''
+    return render_template('not_found.html'), 404
