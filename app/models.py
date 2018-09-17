af4e06a0013b6939e7cf9034942955bbd703c790
kevinjared <kevinjaredkip@gmail.com>
2018-09-17 15:13:53 +0300

    classes creation

---
+++ app/models.py
@@ -0,0 +1,22 @@
+class Source:
+    '''
+    class to define source objects
+    '''
+    def __init__(self, id, name, description, url, category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+
+class Article:
+    '''
+    class to define article objects
+    '''
+    def __init__(self, author, title, description, url, image_url, publish_time):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url 
+        self.image_url = image_url
+        self.publish_time = publish_time
